9aa7db384d724023270d46a375eba6259f28a687
Frédéric Michaud <frederic.a.michaud@gmail.com>
2019-06-28 09:27:07 +0200

    Initial Commit

---
+++ coalescence/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  coalescence
+//
+//  Created by Frédéric Michaud on 28.06.19.
+//  Copyright © 2019 Frédéric Michaud. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

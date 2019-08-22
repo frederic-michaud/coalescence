5f10a41a747077982f88d56b9119e4fc191bc01b
Frédéric Michaud <frederic.a.michaud@gmail.com>
2019-08-22 11:03:27 +0200

    Handling correctly error in the parsing part.

--- coalescence/main.cpp
+++ coalescence/main.cpp
@@ -18,7 +18,14 @@ using namespace std;
 
 
 int main(int argc, const char * argv[]) {
-    parameters* my_parameters = new parameters(argc, argv);
+    parameters* my_parameters;
+    try{
+        my_parameters = new parameters(argc, argv);
+    }
+    catch(const char* error){
+        cerr << error;
+        exit(1);
+    }
     for (int index_sequence(0);index_sequence < my_parameters->get_nb_sequence();index_sequence++){
         simulation my_simulation(my_parameters);
         my_simulation.perform_simulation();
